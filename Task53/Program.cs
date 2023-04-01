03c5d5571220880464fd4ae748b5e5e4bdbe358c
Oli <oli.varnavska@gmail.com>
2023-04-01 21:48:34 +0800

    Add Tasks 53,57

---
+++ Task53/Program.cs
@@ -0,0 +1,79 @@
+﻿// Задача 53: Задайте двумерный массив. 
+// Напишите программу, которая поменяет местами 
+// первую и последнюю строку массива.
+
+// // Создаем двумерный массив размером 3x4 и инициализируем его значениями
+//         int[,] array = { {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12} };
+
+//         // Выводим исходный массив
+//         Console.WriteLine("Исходный массив:");
+//         PrintArray(array);
+
+//         // Меняем строки на столбцы, если это возможно
+//         int rows = array.GetLength(0);
+//         int cols = array.GetLength(1);
+//         if (rows == cols) {
+//             for (int i = 0; i < rows; i++) {
+//                 for (int j = i+1; j < cols; j++) {
+//                     int temp = array[i, j];
+//                     array[i, j] = array[j, i];
+//                     array[j, i] = temp;
+//                 }
+//             }
+
+//             // Выводим измененный массив
+//             Console.WriteLine("Измененный массив:");
+//             PrintArray(array);
+//         } else {
+//             Console.WriteLine("Невозможно заменить строки на столбцы!");
+//         }
+    
+
+//     static void PrintArray(int[,] array) {
+//         int rows = array.GetLength(0);
+//         int cols = array.GetLength(1);
+//         for (int i = 0; i < rows; i++) {
+//             for (int j = 0; j < cols; j++) {
+//                 Console.Write(array[i, j] + " ");
+//             }
+//             Console.WriteLine();
+//         }
+//     }
+
+
+Console.Write("Введите количество строк в массиве: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов в массиве: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите индекс строки: ");
+int indexrows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите индекс столбца: ");
+int indexcolumns = Convert.ToInt32(Console.ReadLine());
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+int[,] matrix = new int[rows, columns];
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
---
+++ Task57/Program.cs
@@ -0,0 +1,106 @@
+﻿// Задача 57: Составить частотный словарь элементов 
+// двумерного массива. Частотный словарь содержит 
+// информацию о том, сколько раз встречается элемент 
+// входных данных.
+
+
+
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintArray2(int[] matr, int[] matr1)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        if (matr1[i] != 0)
+        {
+            Console.Write($"Значение {matr[i]} встречается {matr1[i]} раз"); 
+            Console.WriteLine();
+        }
+    }
+
+}
+
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+bool MassiveCheck(int num, int[] matr)
+{
+bool flag = true;
+for (int i = 0; i < matr.Length; i++)
+{
+    if (matr[i] == num)
+    {
+        flag = false;
+        break;
+    }
+}
+return flag;
+}
+
+void CountUniqueDigits(int[,] matr, int[] res1)
+{
+    int k = 0;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (MassiveCheck(matr[i,j], res1))
+            {
+                res1[k] = matr[i,j];
+                k++;
+            }
+        }
+    }
+}
+
+void CountCounts(int[,] matr, int[] res1, int[] res2)
+{
+    for (int k = 0; k < res1.Length; k++)
+    {
+        int count = 0;
+        for (int i = 0; i < matr.GetLength(0); i++)
+        {
+            for (int j = 0; j < matr.GetLength(1); j++)
+            {
+                if (matr[i,j] == res1[k])
+                {
+                    count++;
+                }
+            }
+        }
+        res2[k] = count;
+    }
+}
+
+
+int[,] matrix = new int[3, 4];
+FillArray(matrix);
+PrintArray(matrix);
+int[] res1 = new int [matrix.GetLength(0) * matrix.GetLength(1)];
+int[] res2 = new int [matrix.GetLength(0) * matrix.GetLength(1)];
+
+CountUniqueDigits(matrix, res1);
+// PrintArray2(res1);
+Console.WriteLine();
+CountCounts(matrix, res1, res2);
+PrintArray2(res1, res2);
